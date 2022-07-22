790d1e60ca30414e4c01af23a01703befab2be52
Anai Lopez <anaijocelyn@gmail.com>
2022-07-22 16:49:43 -0700

    shopping cart functions

--- app/models/shopping_cart.py
+++ app/models/shopping_cart.py
@@ -1,5 +1,6 @@
 from .db import db
 
+
 class ShoppingCart(db.Model):
     __tablename__ = "shopping_carts"
 
@@ -8,5 +9,26 @@ class ShoppingCart(db.Model):
     total = db.Column(db.Float, nullable=False)
 
     user = db.relationship("User", back_populates="shopping_cart")
-    cart_items= db.relationship("CartItem", back_populates="shopping_cart")
+    cart_items = db.relationship("CartItem", back_populates="shopping_cart")
 
+    def __init__(self):
+        self.shoppingList = []
+
+    def addItem(self, item):
+        self.shoppingList.append(item)
+
+    def removeItem(self, item):
+        self.shoppingList.remove(item)
+
+    def getTotal(self):
+        total = 0
+        for item in self.shoppingList:
+            name, price = item
+            total += price
+        return total
+
+    def getShoppingList(self):
+        items = None
+        for item in self.getShoppingList:
+            items += item
+        return items
