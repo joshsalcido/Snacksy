8ff0154e0ec5edef860951b3e58bc41a84a285b2
Mineh <minehgharabegi@gmail.com>
2022-07-21 21:49:39 -0700

    User seeder data updated

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,11 +4,11 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password', first_name='demo', last_name='demo', address='123')
+        username='Demo', email='demo@aa.io', password='password', first_name='Demo', last_name='User', address='12345 Demo Street, Demo, CA 90210')
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password', first_name='marnie', last_name='marnie', address='2323')
+        username='marnie', email='marnie@aa.io', password='password', first_name='Marnie', last_name='Smith', address='2323 Brand Blvd, Miami, FL 33101')
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password', first_name='bobbie', last_name='thebulder', address='222')
+        username='bobbie', email='bobbie@aa.io', password='password', first_name='Bobbie', last_name='Hanson', address='222 Central Ave, Austin, TX 73301')
 
     db.session.add(demo)
     db.session.add(marnie)
