2f284547703ad65db797bb32f03d48b00910daf6
Anai Lopez <anaijocelyn@gmail.com>
2022-07-22 11:12:27 -0700

    signup modal done

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,32 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import LoginForm from './components/auth/LoginForm';
-import SignUpForm from './components/auth/SignUpForm';
 import NavBar from './components/NavBar';
-import ProtectedRoute from './components/auth/ProtectedRoute';
+// import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
 import { authenticate } from './store/session';
-import Modal from 'react-modal';
+
 
 function App() {
   const [loaded, setLoaded] = useState(false);
-  const [showLoginForm, setShowLoginForm] = useState(false)
+
   const dispatch = useDispatch();
-
-  const formStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-    },
-  };
-
-  Modal.setAppElement('body');
 
   useEffect(() => {
     (async () => {
@@ -39,37 +24,22 @@ function App() {
     return null;
   }
 
-  function openModal() {
-    setShowLoginForm(true)
-  }
 
-  function closeModal() {
-    setShowLoginForm(false)
-  }
 
   return (
     <BrowserRouter>
       <NavBar />
       <Switch>
-        <Route path='/login' exact={true}>
-          <button onClick={openModal}>Open</button>
-          <Modal isOpen={showLoginForm} style={formStyles}>
-            <LoginForm />
-            <button onClick={closeModal}>Cancel</button>
-          </Modal>
-        </Route>
-        <Route path='/sign-up' exact={true}>
-          <SignUpForm />
-        </Route>
-        <ProtectedRoute path='/users' exact={true} >
+        <Route path='/users' exact={true} >
           <UsersList />
-        </ProtectedRoute>
-        <ProtectedRoute path='/users/:userId' exact={true} >
+        </Route>
+        <Route path='/users/:userId' exact={true} >
           <User />
-        </ProtectedRoute>
-        <ProtectedRoute path='/' exact={true} >
+        </Route>
+        <Route path='/' exact={true} >
           <h1>My Home Page</h1>
-        </ProtectedRoute>
+
+        </Route>
       </Switch>
     </BrowserRouter>
   );
--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,9 +1,44 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import LoginForm from './auth/LoginForm';
+import SignUpForm from './auth/SignUpForm';
+import Modal from 'react-modal';
 
 const NavBar = () => {
+  const [showLoginForm, setShowLoginForm] = useState(false);
+  const [showSignupForm, setShowSignupForm] = useState(false);
+
+  Modal.setAppElement('body');
+
+  function openLoginModal() {
+    setShowLoginForm(true)
+  }
+
+  function closeLoginModal() {
+    setShowLoginForm(false)
+  }
+
+  function openSignupModal() {
+    setShowSignupForm(true)
+  }
+
+  function closeSignupModal() {
+    setShowSignupForm(false)
+  }
+
+  const formStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
   return (
     <nav>
       <ul>
@@ -13,6 +48,18 @@ const NavBar = () => {
           </NavLink>
         </li>
         <li>
+          <button onClick={openLoginModal}>Log In</button>
+          <Modal isOpen={showLoginForm} style={formStyles}>
+            <LoginForm />
+            <button onClick={closeLoginModal}>Cancel</button>
+            <button onClick={openSignupModal}>Sign up</button>
+          </Modal>
+          <Modal isOpen={showSignupForm} style={formStyles}>
+            <SignUpForm />
+            <button onClick={closeSignupModal}>Cancel</button>
+          </Modal>
+        </li>
+        {/* <li>
           <NavLink to='/login' exact={true} activeClassName='active'>
             Login
           </NavLink>
@@ -21,7 +68,7 @@ const NavBar = () => {
           <NavLink to='/sign-up' exact={true} activeClassName='active'>
             Sign Up
           </NavLink>
-        </li>
+        </li> */}
         {/* <li>
           <NavLink to='/users' exact={true} activeClassName='active'>
             Users
