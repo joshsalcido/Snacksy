8488896d020a675e1f48d69a95e6b55d681d1783
dasch22 <michaeldasch22@gmail.com>
2022-07-29 14:18:58 -0700

    categories styling

--- react-app/src/components/AllSnacks/snacks.js
+++ react-app/src/components/AllSnacks/snacks.js
@@ -29,25 +29,25 @@ const AllSnacks = () => {
                     </li>
                     <li className="category-list">
                         <Link to="/categories/Candy">
-                            <img className="category-button-candy"src="https://i.etsystatic.com/11465033/r/il/90f371/3963233212/il_794xN.3963233212_ib58.jpg" />
+                            <img className="category-button"src="https://i.etsystatic.com/11465033/r/il/90f371/3963233212/il_794xN.3963233212_ib58.jpg" />
                             <p id='category-bttn-name'>Candy</p>
                         </Link>
                     </li>
                     <li className="category-list">
                         <Link to="/categories/Baked Goods">
-                            <img className="category-button-candy"src="https://i.etsystatic.com/11465033/r/il/2062b1/3532352007/il_1140xN.3532352007_4a7r.jpg" />
+                            <img className="category-button"src="https://i.etsystatic.com/11465033/r/il/2062b1/3532352007/il_1140xN.3532352007_4a7r.jpg" />
                             <p id='category-bttn-name'>Baked Goods</p>
                         </Link>
                     </li>
                     <li className="category-list">
                         <Link to="/categories/Protein">
-                            <img className="category-button-candy"src="https://i.etsystatic.com/11465033/r/il/aa3aa6/3963194926/il_794xN.3963194926_2a0t.jpg" />
+                            <img className="category-button"src="https://i.etsystatic.com/11465033/r/il/aa3aa6/3963194926/il_794xN.3963194926_2a0t.jpg" />
                             <p id='category-bttn-name'>Protein</p>
                         </Link>
                     </li>
                     <li className="category-list">
                         <Link to="/categories/Beverage">
-                            <img className="category-button-candy"src="https://i.etsystatic.com/11465033/r/il/6007c8/3808870767/il_1140xN.3808870767_29ot.jpg" />
+                            <img className="category-button"src="https://i.etsystatic.com/11465033/r/il/6007c8/3808870767/il_1140xN.3808870767_29ot.jpg" />
                             <p id='category-bttn-name'>Beverages</p>
                         </Link>
                     </li>
@@ -58,7 +58,7 @@ const AllSnacks = () => {
                 <div key={snack.id} className='snack-card'>
                     <Link className="home-page-snack-card"to={`/snacks/${snack.id}`}>
                         <img id="snack-image"src={snack.cover_pic} alt="snackImg"></img>
-                        <p>{snack.title}</p>
+                        <p id='snack-title'>{snack.title}</p>
                         <p id='snack-price'>$ {snack.price.toFixed(2)}</p>
                     </Link>
                 </div>
